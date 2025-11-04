0a288949c1d1e6301c5ae44ee55744d48cb85d8e
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2025-11-05 00:39:18 +0100

    exceptions part 1

---
+++ backend/src/main/java/com/mohadev/InventoryManagementSystem/exceptions/CustomAccessDeniedHandler.java
@@ -0,0 +1,9 @@
+package com.mohadev.InventoryManagementSystem.exceptions;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler{
+}
