c193558b75c20e495a67215617644add50ab68a1
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2025-11-09 18:45:27 +0100

    exceptions part 2

--- backend/src/main/java/com/mohadev/InventoryManagementSystem/exceptions/CustomAuthenticationEntryPoint.java
+++ backend/src/main/java/com/mohadev/InventoryManagementSystem/exceptions/CustomAuthenticationEntryPoint.java
@@ -7,28 +7,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private final ObjectMapper objectMapper;
 
-
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         Response errorResponse = Response.builder()
-                .status(HttpStatus.FORBIDDEN.value())
-                .message(accessDeniedException.getMessage())
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message(authException.getMessage())
                 .build();
 
         response.setContentType("application/json");
-        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
