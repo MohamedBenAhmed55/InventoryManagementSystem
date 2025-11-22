aa1acac3efcffb86f78a17730a93f881dd82d8af
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2025-11-22 23:16:34 +0100

    login page

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { ApiService } from '../service/api.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private apiService: ApiService, private router: Router) {}
 
+  formData: any = {
+    email: '',
+    password: ''
+  };
+
+  message: string | null = null;
+
+  async handleSumbit() {
+    if(!this.formData.email || !this.formData.password) {
+      this.showMessage("All fields are required");
+      return;
+    }
+
+    try{
+      const response : any = await firstValueFrom(
+        this.apiService.loginUser(this.formData)
+      );
+      if(response.status === 200){
+        this.apiService.encryptAndSaveToStorage('token', response.token);
+        this.apiService.encryptAndSaveToStorage('role', response.role);
+        this.router.navigate(['/dashboard']);
+      }
+    } catch (error: any){
+      console.log(error);
+      this.showMessage(error?.error?.message || error?.message || "Unable to login" + error);
+    }
+
+  }
+
+  showMessage(msg: string) {
+    this.message = msg;
+    setTimeout(() => {
+      this.message = null;
+    }, 4000);
+    
+  }
 }
