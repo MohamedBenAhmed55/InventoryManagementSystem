e8e3f6761ba630855ec44d4733306e8f021d404a
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2025-11-23 21:16:21 +0100

    product page

--- backend/src/main/java/com/mohadev/InventoryManagementSystem/service/impl/ProductServiceImpl.java
+++ backend/src/main/java/com/mohadev/InventoryManagementSystem/service/impl/ProductServiceImpl.java
@@ -33,12 +33,14 @@ public class ProductServiceImpl implements ProductService {
     private static final String IMAGE_DIRECTORY = System.getProperty("user.dir") + "/product-image/";
 
     //AFTER YOUR FROTEND IS SET UP WROTE THIS SO THE IMAGE IS SAVED IN YOUR FRONTEND PUBLIC FOLDER
-//    private static final String IMAGE_DIRECTOR_FRONTEND = "/Users/dennismac/phegonDev/ims-angular/public/products/";
+    private static final String IMAGE_DIRECTOR_FRONTEND = "C:/Users/Mohamed/Documents/springboot-angular/InventoryManagementSystem/frontend/public/products";
+
 
     @Override
-    public Response saveProduct(ProductDTO productDTO, MultipartFile imageFile) throws IllegalAccessException {
+    public Response saveProduct(ProductDTO productDTO, MultipartFile imageFile) {
+
         Category category = categoryRepository.findById(productDTO.getCategoryId())
-                .orElseThrow(()-> new NotFoundException("Category Not Found"));
+                .orElseThrow(() -> new NotFoundException("Category Not Found"));
 
         //map out product dto to product entity
         Product productToSave = Product.builder()
@@ -50,14 +52,13 @@ public class ProductServiceImpl implements ProductService {
                 .category(category)
                 .build();
 
-        if (imageFile != null){
-            String imagePath = saveImage(imageFile);
+        if (imageFile != null) {
+            String imagePath = saveImageToFrontendPublicFolder(imageFile);
             productToSave.setImageUrl(imagePath);
         }
 
         //save the product to our database
         productRepository.save(productToSave);
-
         return Response.builder()
                 .status(200)
                 .message("Product successfully saved")
@@ -68,40 +69,40 @@ public class ProductServiceImpl implements ProductService {
     public Response updateProduct(ProductDTO productDTO, MultipartFile imageFile) {
 
         Product existingProduct = productRepository.findById(productDTO.getProductId())
-                .orElseThrow(()-> new NotFoundException("Product Not Found"));
+                .orElseThrow(() -> new NotFoundException("Product Not Found"));
 
         //check if image is associated with the update request
-        if (imageFile != null && !imageFile.isEmpty()){
-//            String imagePath = saveImageToFrontendPublicFolder(imageFile);
-//            existingProduct.setImageUrl(imagePath);
+        if (imageFile != null && !imageFile.isEmpty()) {
+            String imagePath = saveImageToFrontendPublicFolder(imageFile);
+            existingProduct.setImageUrl(imagePath);
         }
         //Check if category is to be changed for the product
-        if (productDTO.getCategoryId() != null && productDTO.getCategoryId() > 0){
+        if (productDTO.getCategoryId() != null && productDTO.getCategoryId() > 0) {
 
             Category category = categoryRepository.findById(productDTO.getCategoryId())
-                    .orElseThrow(()-> new NotFoundException("Category Not Found"));
+                    .orElseThrow(() -> new NotFoundException("Category Not Found"));
             existingProduct.setCategory(category);
         }
 
         //check and update fiedls
 
-        if (productDTO.getName() !=null && !productDTO.getName().isBlank()){
+        if (productDTO.getName() != null && !productDTO.getName().isBlank()) {
             existingProduct.setName(productDTO.getName());
         }
 
-        if (productDTO.getSku() !=null && !productDTO.getSku().isBlank()){
+        if (productDTO.getSku() != null && !productDTO.getSku().isBlank()) {
             existingProduct.setSku(productDTO.getSku());
         }
 
-        if (productDTO.getDescription() !=null && !productDTO.getDescription().isBlank()){
+        if (productDTO.getDescription() != null && !productDTO.getDescription().isBlank()) {
             existingProduct.setDescription(productDTO.getDescription());
         }
 
-        if (productDTO.getPrice() !=null && productDTO.getPrice().compareTo(BigDecimal.ZERO) >=0){
+        if (productDTO.getPrice() != null && productDTO.getPrice().compareTo(BigDecimal.ZERO) >= 0) {
             existingProduct.setPrice(productDTO.getPrice());
         }
 
-        if (productDTO.getStockQuantity() !=null && productDTO.getStockQuantity() >=0){
+        if (productDTO.getStockQuantity() != null && productDTO.getStockQuantity() >= 0) {
             existingProduct.setStockQuantity(productDTO.getStockQuantity());
         }
 
@@ -119,7 +120,8 @@ public class ProductServiceImpl implements ProductService {
 
         List<Product> products = productRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
 
-        List<ProductDTO> productDTOS = modelMapper.map(products, new TypeToken<List<ProductDTO>>() {}.getType());
+        List<ProductDTO> productDTOS = modelMapper.map(products, new TypeToken<List<ProductDTO>>() {
+        }.getType());
 
         return Response.builder()
                 .status(200)
@@ -132,7 +134,7 @@ public class ProductServiceImpl implements ProductService {
     public Response getProductById(Long id) {
 
         Product product = productRepository.findById(id)
-                .orElseThrow(()-> new NotFoundException("Product Not Found"));
+                .orElseThrow(() -> new NotFoundException("Product Not Found"));
 
 
         return Response.builder()
@@ -146,7 +148,7 @@ public class ProductServiceImpl implements ProductService {
     public Response deleteProduct(Long id) {
 
         productRepository.findById(id)
-                .orElseThrow(()-> new NotFoundException("Product Not Found"));
+                .orElseThrow(() -> new NotFoundException("Product Not Found"));
 
         productRepository.deleteById(id);
 
@@ -156,57 +158,59 @@ public class ProductServiceImpl implements ProductService {
                 .build();
     }
 
-//    private String saveImageToFrontendPublicFolder(MultipartFile imageFile){
-//        //validate image check
-//        if (!imageFile.getContentType().startsWith("image/")){
-//            throw new IllegalArgumentException("Only image files are allowed");
-//        }
-//        //create the directory to store images if it doesn't exist
-//        File directory = new File(IMAGE_DIRECTOR_FRONTEND);
-//
-//        if (!directory.exists()){
-//            directory.mkdir();
-//            log.info("Directory was created");
-//        }
-//        //generate unique file name for the image
-//        String uniqueFileName = UUID.randomUUID() + "_" + imageFile.getOriginalFilename();
-//        //get the absolute path of the image
-//        String imagePath = IMAGE_DIRECTOR_FRONTEND + uniqueFileName;
-//
-//        try {
-//            File desctinationFile = new File(imagePath);
-//            imageFile.transferTo(desctinationFile); //we are transfering(writing to this folder)
-//
-//        }catch (Exception e){
-//            throw new IllegalArgumentException("Error occurend while saving image" + e.getMessage());
-//        }
-//
-//        return "products/"+uniqueFileName;
-//    }
-
-    private String saveImage(MultipartFile imageFile) throws IllegalAccessException {
-        if(!imageFile.getContentType().startsWith("image/")){
-            throw new IllegalAccessException("Only image files are allowed");
+    private String saveImageToFrontendPublicFolder(MultipartFile imageFile) {
+        //validate image check
+        if (!imageFile.getContentType().startsWith("image/")) {
+            throw new IllegalArgumentException("Only image files are allowed");
         }
+        //create the directory to store images if it doesn't exist
+        File directory = new File(IMAGE_DIRECTOR_FRONTEND);
 
-        File directory = new File(IMAGE_DIRECTORY);
-
-        if(!directory.exists()){
+        if (!directory.exists()) {
             directory.mkdir();
             log.info("Directory was created");
         }
-
+        //generate unique file name for the image
         String uniqueFileName = UUID.randomUUID() + "_" + imageFile.getOriginalFilename();
+        //get the absolute path of the image
+        String imagePath = IMAGE_DIRECTOR_FRONTEND + uniqueFileName;
+
+        try {
+            File desctinationFile = new File(imagePath);
+            imageFile.transferTo(desctinationFile); //we are transfering(writing to this folder)
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error occurend while saving image" + e.getMessage());
+        }
+
+        return "products/" + uniqueFileName;
+    }
+
+    private String saveImage(MultipartFile imageFile) {
+        //validate image check
+        if (!imageFile.getContentType().startsWith("image/")) {
+            throw new IllegalArgumentException("Only image files are allowed");
+        }
+        //create the directory to store images if it doesn't exist
+        File directory = new File(IMAGE_DIRECTORY);
+
+        if (!directory.exists()) {
+            directory.mkdir();
+            log.info("Directory was created");
+        }
+        //generate unique file name for the image
+        String uniqueFileName = UUID.randomUUID() + "_" + imageFile.getOriginalFilename();
+        //get the absolute path of the image
         String imagePath = IMAGE_DIRECTORY + uniqueFileName;
 
-        try{
-            File destinationFile = new File(imagePath);
-            imageFile.transferTo(destinationFile); // write in the folder
-        }catch (Exception e){
-            throw new IllegalArgumentException("Error occured while saving image" + e.getMessage());
+        try {
+            File desctinationFile = new File(imagePath);
+            imageFile.transferTo(desctinationFile); //we are transfering(writing to this folder)
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error occurend while saving image" + e.getMessage());
         }
 
         return imagePath;
-
     }
 }
